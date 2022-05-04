e5e6a53506a44a1fbd97bd76663175f8e451fae8
AdityaShidlyali <adityamshidlyali@gmail.com>
2022-05-04 20:16:59 +0530

    Camera integration in android

---
+++ app/src/main/java/com/adityashidlyali/cameraintegration/MainActivity.kt
@@ -0,0 +1,137 @@
+package com.adityashidlyali.cameraintegration
+
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var photoFile: File
+
+    private val FILE_NAME = "photo.jpg"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        val imageView: ImageView = findViewById(R.id.image_view)
+
+        val resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
+                    imageView.setImageBitmap(takenImage)
+                }
+            }
+
+        button.setOnClickListener {
+//            if (!hasCameraPermission()) {
+//                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 0)
+//            } else {
+//                val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//                photoFile = getPhotoFile(FILE_NAME)
+//
+//                // Make sure there is any application in the android device which can handle this intent
+//                val fileProvider =
+//                    FileProvider.getUriForFile(this, "com.adityashidlyali.camera", photoFile)
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+//
+//                if (takePictureIntent.resolveActivity(this.packageManager) != null) {
+//                    resultLauncher.launch(takePictureIntent)
+//                } else {
+//                    Toast.makeText(this, "Unable to open camera", Toast.LENGTH_SHORT).show()
+//                }
+//            }
+
+            requestPermissions()
+        }
+    }
+
+    private fun getPhotoFile(fileName: String): File {
+        // use 'getExternalFilesDir' on context to access package-specific directories
+        val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName, ".jpg", storageDirectory)
+    }
+
+    private fun requestPermissions() {
+        val permissionsToRequest = mutableListOf<String>()
+        if (!hasCameraPermission()) {
+            permissionsToRequest.add(Manifest.permission.CAMERA)
+        }
+
+        if (!hasReadExternalStoragePermission()) {
+            permissionsToRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+
+        if (!hasWriteExternalStoragePermission()) {
+            permissionsToRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+
+        if (!hasLocationForegroundPermission()) {
+            permissionsToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+
+        if (!hasLocationBackgroundPermission()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                permissionsToRequest.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            }
+        }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), 0)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 0 && grantResults.isNotEmpty()) {
+            for (i in grantResults.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(this.javaClass.simpleName, permissions[i])
+                }
+            }
+        }
+    }
+
+    private fun hasCameraPermission() = ActivityCompat.checkSelfPermission(
+        this, Manifest.permission.CAMERA
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasReadExternalStoragePermission() = ActivityCompat.checkSelfPermission(
+        this, Manifest.permission.READ_EXTERNAL_STORAGE
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasWriteExternalStoragePermission() = ActivityCompat.checkSelfPermission(
+        this, Manifest.permission.WRITE_EXTERNAL_STORAGE
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasLocationForegroundPermission() = ActivityCompat.checkSelfPermission(
+        this, Manifest.permission.ACCESS_COARSE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasLocationBackgroundPermission() =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+}
